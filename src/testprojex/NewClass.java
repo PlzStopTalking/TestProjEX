d448cccf0c7b3675ac32e07abe7bc1ec60258187
PlzStopTalking <Farishamdi05@gmail.com>
2021-12-01 08:36:34 -0800

    Initial commit

---
+++ src/testprojex/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojex;
+
+/**
+ *
+ * @author 384001033
+ */
+public class NewClass {
+    //the voices get louder
+}
---
+++ src/testprojex/TestProjEX.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojex;
+
+/**
+ *
+ * @author 384001033
+ */
+public class TestProjEX {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    //pain
+    
+}
